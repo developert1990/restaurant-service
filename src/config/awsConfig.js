ef3a2d26bf551752a8baa183190c8c589286cdf7
developert1990 <developert.hong@gmail.com>
2021-08-06 02:39:02 -0600

    changed > to >>

--- src/config/awsConfig.js
+++ src/config/awsConfig.js
@@ -3,9 +3,9 @@ const AWS = require('aws-sdk');
 
 const initialAWS = () => {
     const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, PORT } = process.env;
-    // if (!AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) {
-    //     throw new Error('AWS_ACCESS_KEY or SECRET_KEY is not set');
-    // }
+    if (!AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) {
+        throw new Error('AWS_ACCESS_KEY or SECRET_KEY is not set');
+    }
     AWS.config.update({
         region: 'us-east-1',
     });
